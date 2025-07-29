e9dae3b9260c1e106766b488ff1775e51e1a2993
samirvithlani <samir.vithlani83955@gmail.com>
2025-07-29 19:49:53 +0530

    add

---
+++ basics/basic1.cpp
@@ -0,0 +1,23 @@
+/*
+    Basic prog of introduction...
+*/
+
+//#header file
+//# --->
+//<iostream.h>
+//input stream class
+//ouput stream class
+
+#include<iostream>
+using namespace std;
+
+//printf("") function
+int main(){
+    //obejct
+    //cout -- is not a function
+    //cout is referance variable or object of output stream class..
+    std::cout<<"hello this is my first prg using cpp";
+    cout<<"\n hi this is second line without std:: ";
+
+
+}
---
+++ basics/basic2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+
+    int a = 100;
+    int no1=10,no2=20;
+    cout<<a;
+    cout<<"\nvalue of a = "<<a;
+    cout<<"\nvalue of no1 = "<<no1<<"\nvalue of no2 = "<<no2;
+
+}
